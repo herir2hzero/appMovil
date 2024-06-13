111d43f60a13b686cfe83212e8b6f7ab6476cad7
herir2hzero <168882024+herir2hzero@users.noreply.github.com>
2024-06-12 19:45:25 -0700

    Update app.py

--- src/app.py
+++ src/app.py
@@ -7,18 +7,16 @@ from routes.to_do import to_do
 from routes.documentos import documentos
 
 load_dotenv()
-app = Flask(__name__)
+def crear_app():
+    app = Flask(__name__)
 
-app.config['MONGO_URI'] = os.getenv('MONGO_URI')
-mongo.init_app(app)
+    app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+    mongo.init_app(app)
 
-
-@app.route('/')
-def index():
-    return 'Hello World'
-
-app.register_blueprint(to_do, url_prefix='/to_do')
-app.register_blueprint(documentos, url_prefix='/documentos')
+    app.register_blueprint(to_do, url_prefix='/to_do')
+    app.register_blueprint(documentos, url_prefix='/documentos')
+    return app
 
 if __name__ == '__main__':
+    app = crear_app()
     app.run(debug=True, host='0.0.0.0')
